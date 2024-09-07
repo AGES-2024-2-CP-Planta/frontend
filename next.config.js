b915f4aa7f9ae0ebae623cbce335390427cd82a5
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2024-09-07 19:23:09 -0300

    test: fix files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
-export const reactStrictMode = true;
-export const env = {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-};
-export const images = {
-    domains: ['example.com'],
-};
-export function webpack(config, { isServer }) {
-    return config;
-}
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
+    images: {
+      domains: ['example.com'],
+    },
+    webpack: (config, { isServer }) => {
+      return config;
+    }
+  };
+  
+  module.exports = nextConfig;
   
