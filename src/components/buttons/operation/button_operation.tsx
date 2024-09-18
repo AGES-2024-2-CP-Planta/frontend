96c62e9b156487e6b6aa46f7751614a59accbafb
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-09-17 23:19:18 -0300

    feat: button now receives a href to redirect

--- src/components/buttons/operation/button_operation.tsx
+++ src/components/buttons/operation/button_operation.tsx
@@ -1,18 +1,27 @@
-import AddIcon from '../../icons/add-icon.svg'
+import Link from 'next/link';
+import AddIcon from '../../icons/add-icon.svg';
 
 type ButtonOperationProps = {
-    title: string
-    description: string
-}
+  title: string;
+  description: string;
+  href: string;
+};
 
-export default function ButtonOperation({title, description}: ButtonOperationProps) {
-    return (
-        <button className="border-[#006F48] border-2 rounded w-1/4 h-24 flex justify-start p-6 gap-x-4">
-            <img className='w-6' src={AddIcon.src} alt='Add Icon'/>
-            <div className="flex flex-col text-start gap-y-1">
-                <h3 className='text-lg'>{title}</h3>
-                <p className='text-sm'>{description}</p>
-            </div>
-        </button>
-    )
+export default function ButtonOperation({
+  title,
+  description,
+  href
+}: ButtonOperationProps) {
+  return (
+    <Link
+      href={href}
+      className="border-[#006F48] border-2 rounded-lg w-full h-auto flex items-start p-6 gap-x-4"
+    >
+      <img className="w-6" src={AddIcon.src} alt="Add Icon" />
+      <div className="flex flex-col text-start gap-y-1">
+        <h3 className="text-xl font-bold">{title}</h3>
+        <p className="text-[#757575] text-sm">{description}</p>
+      </div>
+    </Link>
+  );
 }
