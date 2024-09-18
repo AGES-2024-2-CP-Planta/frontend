d759c12e285b8d076ab35bfa2264ef32186e0761
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-09-17 23:20:54 -0300

    feat: adding path to button redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
-import Image from 'next/image';
 import ButtonOperation from '../components/buttons/operation/button_operation';
 
 export default function Home() {
   return (
     <main>
-      <div className='flex gap-x-6 justify-center'>
-        <ButtonOperation title="Criar Operação" description="Selecione Etapas para a Produção" />
-        <ButtonOperation title="Cadastrar Matéria Prima" description="Cadastro de Matérias Primas para Operação" />
-        <ButtonOperation title="Cadastrar Produto Final" description="Cadastro de Produtos Finais" />
+      <div className="flex space-x-6 justify-center mx-9">
+        <ButtonOperation
+          title="Criar Operação"
+          description="Selecione Etapas para a Produção"
+          href="/operation/create"
+        />
+        <ButtonOperation
+          title="Cadastrar Matéria Prima"
+          description="Cadastro de Matérias Primas para Operação"
+          href="/raw-material/create"
+        />
+        <ButtonOperation
+          title="Cadastrar Produto Final"
+          description="Cadastro de Produtos Finais"
+          href="/final-product/create"
+        />
       </div>
     </main>
   );
