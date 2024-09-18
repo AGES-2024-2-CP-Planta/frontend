94bb34ced60d85ad71028805a35e65fb4c619805
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-09-17 23:17:52 -0300

    feat: creating routes for home buttons

---
+++ src/app/operation/create/page.tsx
@@ -0,0 +1,10 @@
+
+export default function OpeartionCreate() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p>operation</p>
+      </div>
+    </main>
+  );
+}
